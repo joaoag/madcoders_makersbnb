4677ca6fa32c83ec2ab7d01438114cf82b57843f
Alina Goaga <goaga.alina@gmail.com>
2019-01-09 21:52:45 +0000

    Venue created and added to the list of venues on the profile page

--- lib/user.rb
+++ lib/user.rb
@@ -7,14 +7,24 @@ class User
   property :email, String
   property :password, BCryptHash
 
+  has n, :venues
+
   def self.authenticate(email, password)
     user = first(email: email)
     return nil unless user
 
-    if user.password == password
-      user
-    else
-      nil
-    end
+    user if user.password == password
   end
+end
+
+class Venue
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :address, String
+  property :ppn, Integer
+  property :description, Text
+
+  belongs_to :user
 end
