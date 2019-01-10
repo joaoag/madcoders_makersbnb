994b5757df4a3b121b344fe6abbb53d07cc9ad99
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-01-10 12:05:31 +0000

    Added webhelpers to DRY up code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ ENV['RACK_ENV'] ||= 'test'
 
 require 'capybara/rspec'
 require 'database_cleaner'
-
+require './spec/features/web_helpers.rb'
 require './app'
 require './spec/support/database_cleaner'
 require 'simplecov'
