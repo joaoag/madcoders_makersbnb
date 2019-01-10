2e93de56350de05fb45ced8887c59ee839c01553
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-01-10 15:00:13 +0000

    removing unneccessary method sign_up_sign_in from web helpers

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -13,15 +13,3 @@ def sign_in
   fill_in :password, with: 'secret123'
   click_button 'Sign in'
 end
-
-def sign_up_sign_in
-  visit '/'
-  click_on 'Sign up'
-  fill_in :email, with: 'test@test.com'
-  fill_in :password, with: 'secret123'
-  click_button 'Sign up'
-  click_on 'Sign in'
-  fill_in :email, with: 'test@test.com'
-  fill_in :password, with: 'secret123'
-  click_button 'Sign in'
-end
